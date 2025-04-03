5c3b3a293267367a51cfe9a5d6e7c16e260c17d3
valentinazag <valentinazagman@gmail.com>
2025-04-03 18:22:23 -0300

    Primer submit

---
+++ app/config.php
@@ -0,0 +1,21 @@
+
+<?php
+$URL= "http://localhost/documents/VETERINARIA/";
+
+define('APP_NAME','SISTEMA VETERINARIO- ZV'); //variable global que contiene el nombre del sistema
+define('SERVIDOR','localhost');  //en el momento de hostear ne otro lado aca va la ip, ruta, etc
+define('USUARIO','root');  
+define('PASSWORD',''); 
+define('BD','veterinaria'); 
+
+$servidor= "mysql:bdname=".BD.";host:".SERVIDOR;
+
+try {
+    $pdp = new PDO($servidor,USUARIO,PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+    echo "Conexion exitosa a BD";
+} catch (PDOException $e) {
+    print_r($e);
+    echo "No se ha podido conectar a la BD";
+}
+
+?>
---
+++ login/login.php
@@ -0,0 +1,69 @@
+<?php
+include("../app/config.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo APP_NAME?></title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo $URL;?>/public/templates/AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?php echo $URL;?>/public/templates/AdminLTE-3.2.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo $URL;?>/public/templates/AdminLTE-3.2.0/dist/css/adminlte.min.css">
+   <!-- CSS -->
+  <link rel="stylesheet" href="../public/css/login.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box"> 
+  <div class="login-logo">
+    <a href="<?php echo $URL;?>/public/templates/AdminLTE-3.2.0/index2.html"><b>INICIAR SESIÓN</b></a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg"><b>Ingresa tus datos </b></p>
+      <hr>
+      <form action="<?php echo $URL;?>/public/templates/AdminLTE-3.2.0/index3.html" method="post">
+        <label for="">Correo electrónico</label>
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <label for="">Contraseña</label>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <button class="btn btn-primary" style="width:100%;";>Ingresar</button>
+        <br><br>
+        <button class="btn btn-secondary" style="width:100%;";>Cancelar</button>
+      </form>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="<?php echo $URL;?>/public/templates/AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo $URL;?>/public/templates/AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo $URL;?>/public/templates/AdminLTE-3.2.0/dist/js/adminlte.min.js"></script>
+</body>
+</html>
